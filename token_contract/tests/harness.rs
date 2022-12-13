f0184931774466b461476a0ffe70aa4139dd85b9
alexey <alexnagornnyy.an@gmail.com>
2022-12-13 17:49:34 +0000

    :white_check_mark: :recycle: test token contract refactored

--- token_contract/tests/harness.rs
+++ token_contract/tests/harness.rs
@@ -1,108 +1,275 @@
-contract;
-
-use std::{
-    address::*,
-    revert::require,
-    context::{*, call_frames::*},
-    contract_id::ContractId,
-    storage::*,
-    token::*,
+use fuels::{
+    prelude::*,
+    tx::{AssetId, ContractId},
 };
 
-use token_abi::Token;
-use swayswap_helpers::get_msg_sender_address_or_panic;
+///////////////////////////////
+// Load the Token Contract abi
+///////////////////////////////
+abigen!(TestToken, "out/debug/token_contract-abi.json");
 
-const ZERO_B256 = 0x0000000000000000000000000000000000000000000000000000000000000000;
+#[tokio::test]
+async fn token_contract() {
+    // default initial amount 1000000000
+    let initial_amount = 1000000000;
+    let num_wallets = 3;
+    let num_coins = 1;
+    let config = WalletsConfig::new(Some(num_wallets), Some(num_coins), Some(initial_amount));
+    let mut wallets = launch_custom_provider_and_get_wallets(config, None, None).await;
 
-storage {
-    owner: Address = Address { value: 0x0000000000000000000000000000000000000000000000000000000000000000 },
-    mint_amount: u64 = 0,
-    mint_list: StorageMap<Address, bool> = StorageMap {},
-}
+    let wallet_owner = wallets.get(0).unwrap();
+    let wallet_mint1 = wallets.get(1).unwrap();
+    let wallet_mint2 = wallets.get(2).unwrap();
 
-enum Error {
-    AddressAlreadyMint: (),
-    CannotReinitialize: (),
-    MintIsClosed: (),
-    NotOwner: (),
-}
+    ////////////////////////////////////////////////////////
+    // Setup contracts
+    ////////////////////////////////////////////////////////
 
-#[storage(read)]
-fn validate_owner() {
-    let sender = get_msg_sender_address_or_panic();
-    require(storage.owner == sender, Error::NotOwner);
-}
+    let token_contract_id = Contract::deploy(
+        "out/debug/token_contract.bin",
+        &wallet_owner,
+        TxParameters::default(),
+        StorageConfiguration::with_storage_path(Some(
+            "./out/debug/token_contract-storage_slots.json".to_string(),
+        )),
+    )
+    .await
+    .unwrap();
+    let token_instance = TestToken::new(token_contract_id.clone(), wallet_owner.clone());
 
-impl Token for Contract {
-    //////////////////////////////////////
-    // Owner methods
-    //////////////////////////////////////
-    #[storage(read, write)]
-    fn initialize(mint_amount: u64, address: Address) {
-        require(storage.owner.into() == ZERO_B256, Error::CannotReinitialize);
-        // Start the next message to be signed
-        storage.owner = address;
-        storage.mint_amount = mint_amount;
-    }
+    ////////////////////////////////////////////////////////
+    // Test Token Contract
+    ////////////////////////////////////////////////////////
 
-    #[storage(read, write)]
-    fn set_mint_amount(mint_amount: u64) {
-        validate_owner();
-        storage.mint_amount = mint_amount;
-    }
+    // Get the contract ID and a handle to it
+    let token_mint_amount = 10000;
+    // Amount of tokens given to the wallet
+    let wallet_token_amount = 1000;
 
-    #[storage(read)]
-    fn mint_coins(mint_amount: u64) {
-        validate_owner();
-        mint(mint_amount);
-    }
+    // Initialize contract
+    token_instance
+        .methods()
+        .initialize(token_mint_amount, Address::from(wallet_owner.address()))
+        .call()
+        .await
+        .unwrap();
 
-    #[storage(read)]
-    fn burn_coins(burn_amount: u64) {
-        validate_owner();
-        burn(burn_amount);
-    }
+    // Contract can be initialized only once
+    let is_error = token_instance
+        .methods()
+        .initialize(token_mint_amount, Address::from(wallet_owner.address()))
+        .call()
+        .await
+        .is_err();
+    assert!(is_error);
 
-    #[storage(read)]
-    fn transfer_coins(coins: u64, address: Address) {
-        validate_owner();
-        transfer_to_address(coins, contract_id(), address);
-    }
+    // Verify the mint amount
+    let mint_amount_contract = token_instance
+        .methods()
+        .get_mint_amount()
+        .call()
+        .await
+        .unwrap();
+    assert_eq!(mint_amount_contract.value, token_mint_amount);
 
-    #[storage(read)]
-    fn transfer_token_to_output(coins: u64, asset_id: ContractId, address: Address) {
-        validate_owner();
-        transfer_to_address(coins, asset_id, address);
-    }
+    // Verify update mint amount
+    token_instance
+        .methods()
+        .set_mint_amount(1)
+        .call()
+        .await
+        .unwrap();
+    let mint_amount_contract = token_instance
+        .methods()
+        .get_mint_amount()
+        .call()
+        .await
+        .unwrap();
+    assert_eq!(mint_amount_contract.value, 1);
 
-    //////////////////////////////////////
-    // Mint public method
-    //////////////////////////////////////
-    #[storage(read, write)]
-    fn mint() {
-        require(storage.mint_amount > 0, Error::MintIsClosed);
+    // Update mint amount to the original value
+    token_instance
+        .methods()
+        .set_mint_amount(token_mint_amount)
+        .call()
+        .await
+        .unwrap();
 
-        // Enable a address to mint only once
-        let sender = get_msg_sender_address_or_panic();
-        require(storage.mint_list.get(sender) == false, Error::AddressAlreadyMint);
+    // Mint some alt tokens
+    token_instance
+        .methods()
+        .mint_coins(token_mint_amount)
+        .call()
+        .await
+        .unwrap();
 
-        storage.mint_list.insert(sender, true);
-        mint_to_address(storage.mint_amount, sender);
-    }
+    // Check the balance of the contract of its own asset
+    let result = token_instance.methods().get_balance().call().await.unwrap();
+    assert_eq!(result.value, token_mint_amount);
 
-    //////////////////////////////////////
-    // Read-Only methods
-    //////////////////////////////////////
-    #[storage(read)]
-    fn get_mint_amount() -> u64 {
-        storage.mint_amount
-    }
+    // Transfer tokens to the wallet
+    let address = Address::from(wallet_owner.address());
+    token_instance
+        .methods()
+        .transfer_coins(wallet_token_amount, address.clone())
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .unwrap();
+    // Check the balance of the contract of its own asset
+    let result = token_instance.methods().get_balance().call().await.unwrap();
+    let contract_balance = token_mint_amount - wallet_token_amount;
+    assert_eq!(result.value, contract_balance);
 
-    fn get_balance() -> u64 {
-        balance_of(contract_id(), contract_id())
-    }
+    // Burn all minted coins
+    token_instance
+        .methods()
+        .burn_coins(contract_balance)
+        .call()
+        .await
+        .unwrap();
 
-    fn get_token_balance(asset_id: ContractId) -> u64 {
-        balance_of(asset_id, contract_id())
-    }
+    // Check the balance of the contract of its own asset
+    let result = token_instance.methods().get_balance().call().await.unwrap();
+    assert_eq!(result.value, 0);
+
+    ////////////////////////////////////////////////////////
+    // Test mint and transfer to address
+    ////////////////////////////////////////////////////////
+
+    let token_mint1_instance = TestToken::new(token_contract_id.clone(), wallet_mint1.clone());
+    // Mint and transfer some alt tokens to the wallet
+    token_mint1_instance
+        .methods()
+        .mint()
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .unwrap();
+    // Mint can be called only once
+    let is_error = token_mint1_instance
+        .methods()
+        .mint()
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .is_err();
+    assert!(is_error);
+
+    // Inspect the wallet for alt tokens
+    let alt_token_id = AssetId::from(*token_contract_id.hash());
+    let alt_token_balance = wallet_mint1.get_asset_balance(&alt_token_id).await.unwrap();
+    // The wallet shall received the tokens minted
+    assert_eq!(alt_token_balance, token_mint_amount);
+
+    //  Other wallet should be able to mint tokens
+    let token_mint2_instance = TestToken::new(token_contract_id.clone(), wallet_mint2.clone());
+    token_mint2_instance
+        .methods()
+        .mint()
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .unwrap();
+
+    // Inspect the wallet for alt tokens
+    let alt_token_balance2 = wallet_mint2.get_asset_balance(&alt_token_id).await.unwrap();
+    // The wallet shall received the tokens minted
+    assert_eq!(alt_token_balance2, token_mint_amount);
+
+    // As we mint and transfer the contract balance should be 0
+    let result = token_instance.methods().get_balance().call().await.unwrap();
+    assert_eq!(result.value, 0);
+
+    ////////////////////////////////////////////////////////
+    // Check only owner can call contract
+    ////////////////////////////////////////////////////////
+
+    let is_error = token_mint1_instance
+        .methods()
+        .burn_coins(1)
+        .call()
+        .await
+        .is_err();
+    assert!(is_error);
+    let is_error = token_mint1_instance
+        .methods()
+        .mint_coins(1)
+        .call()
+        .await
+        .is_err();
+    assert!(is_error);
+    let is_error = token_mint1_instance
+        .methods()
+        .set_mint_amount(1)
+        .call()
+        .await
+        .is_err();
+    assert!(is_error);
+    let is_error = token_mint1_instance
+        .methods()
+        .transfer_token_to_output(
+            1,
+            ContractId::from(*token_contract_id.hash()),
+            Address::from(wallet_mint2.address()),
+        )
+        .call()
+        .await
+        .is_err();
+    assert!(is_error);
+
+    ////////////////////////////////////////////////////////
+    // Deposit and transfer ETH on the contract
+    ////////////////////////////////////////////////////////
+
+    let wallet_native_balance_before = wallet_owner
+        .get_asset_balance(&BASE_ASSET_ID)
+        .await
+        .unwrap();
+    let send_native_token_amount = 100;
+
+    // Send native tokens to the contract
+    let contract_native_token_balance = token_instance
+        .methods()
+        .get_token_balance(ContractId::from(*BASE_ASSET_ID))
+        .call_params(CallParameters::new(
+            Some(send_native_token_amount),
+            None,
+            None,
+        ))
+        .call()
+        .await
+        .unwrap();
+    assert_eq!(
+        contract_native_token_balance.value,
+        send_native_token_amount
+    );
+
+    // Check user balance didn't has the sent native tokens
+    let wallet_native_balance_after = wallet_owner
+        .get_asset_balance(&BASE_ASSET_ID)
+        .await
+        .unwrap();
+    assert_eq!(
+        wallet_native_balance_after,
+        wallet_native_balance_before - send_native_token_amount
+    );
+
+    // Transfer coins back to the wallet from the contract
+    token_instance
+        .methods()
+        .transfer_token_to_output(
+            send_native_token_amount,
+            ContractId::from(*BASE_ASSET_ID),
+            Address::from(wallet_owner.address()),
+        )
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .unwrap();
+    let wallet_native_balance_after = wallet_owner
+        .get_asset_balance(&BASE_ASSET_ID)
+        .await
+        .unwrap();
+    assert_eq!(wallet_native_balance_before, wallet_native_balance_after);
 }
